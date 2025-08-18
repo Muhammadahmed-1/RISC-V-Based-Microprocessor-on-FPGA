e676bb31bf79285362f1795758aff81c5cfdd60d
Muhammadahmed-1 <68020566+Muhammadahmed-1@users.noreply.github.com>
2025-08-18 04:55:01 -0400

    Update RF.v

--- RF.v
+++ RF.v
@@ -12,11 +12,23 @@ module RF(RFWrite,regA,regB,regw,clock,reset,dataA,dataB,dataW);
 
     reg [7:0] registers [0:7]; // 8 registers of 8 bits each
 
-    assign dataA = registers[regA];
-    assign dataB = registers[regB];
+    always @(*) begin
+        dataA = registers[regA];
+        dataB = registers[regB];
+    end
 
-    always @(posedge clk) begin
-        if (RFWrite)
-            registers[regW] <= dataW;
+    always @(posedge clock or posedge reset) begin
+        if (reset) begin
+            registers[0] <= 8'b0;
+            registers[1] <= 8'b0;
+            registers[2] <= 8'b0;
+            registers[3] <= 8'b0;
+            registers[4] <= 8'b0;
+            registers[5] <= 8'b0;
+            registers[6] <= 8'b0;
+            registers[7] <= 8'b0;
+        end else if (RFWrite) begin
+            registers[regw] <= dataW;
+        end
     end
 endmodule
